3eda1d33303f62bf42b7f44033a19e5901f3f07b
Ugurcan T <ugurcanturkdogan@gmail.com>
2022-08-27 22:17:10 -0700

    Style fixes

--- src/Template.js
+++ src/Template.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './Template.css';
 
 function Template(props) {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Template from './Template';
 import App from './App';
 
 ReactDOM.render(
